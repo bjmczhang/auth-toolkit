a880eab907a86528ea068cb2927b12080ead4c59
bjmczhang <bjmczhang@gmail.com>
2024-08-02 15:52:34 +1000

    Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
-  return <p className="font-semibold">hello auth!</p>;
+  return <Button>Click Me!</Button>;
 }
